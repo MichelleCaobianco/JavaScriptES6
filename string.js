ca67bde137fd7c90a92eab34f65db39da4dfe78e
MichelleCaobianco <michellecaobianco@gmail.com>
2021-02-08 20:51:12 -0300

    First commit

---
+++ string.js
@@ -0,0 +1,18 @@
+//retorna o tamanho de uma string
+const textSize = 'Teste'.length;
+console.log('Quantidade de letras:',textSize);
+
+//retorna um array quebrando a string por um delimitador
+const splittedText = 'Teste'.split('s');
+console.log('Array com as posições separadas por um delimitador:', splittedText);
+
+//busca por um valor e substitui por outro
+const replacedText = 'Teste'.replace('Teste','Testando');
+console.log('Substitui valor:', replacedText);
+
+//retorna a "fatia" de um valor
+const lastChar = 'Teste'.slice(-1);
+console.log('Última letra de uma string:', lastChar);
+
+const allWithoutLastChar = 'Teste'.slice(0,-1);
+console.log('Todas as letras menos a última:', allWithoutLastChar);
